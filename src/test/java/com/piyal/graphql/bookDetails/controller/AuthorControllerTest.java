9ac7105190476742aabe1cdf42b0502eac03d1c1
Piyal Ahmed <piyal.salamence@gmail.com>
2023-05-11 15:20:37 +0600

    Initial commit:     - create, read and delete operation using spring graphql     - 100% code coverage with test cases

--- src/test/java/com/piyal/graphql/bookDetails/controller/AuthorControllerTest.java
+++ src/test/java/com/piyal/graphql/bookDetails/controller/AuthorControllerTest.java
@@ -10,8 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
